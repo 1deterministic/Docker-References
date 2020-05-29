0e8187d225f2077b88d03998ae56f22d4385af20
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-05-29 01:54:21 -0300

    Added missing 'xhost +'

--- firefox/run.sh
+++ firefox/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+xhost +
 sudo docker run -it --rm --shm-size 1G --device=/dev/dri --group-add video -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/user/1000/pulse --privileged 1d/firefox:latest
--- glxgears/run.sh
+++ glxgears/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+xhost +
 sudo docker run -it --rm --shm-size 1G --device=/dev/dri --group-add video -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged 1d/glxgears:latest
